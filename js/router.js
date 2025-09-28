ae074fa2735c03fd985414af111073c522e9ca57
pxix <139904814+pxix03@users.noreply.github.com>
2025-09-28 23:32:41 +0900

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,13 +1,18 @@
-// #/store, #/cart, #/login ... 경로를 간단 매핑
 const routes = {
-  '':        'home',
-  'store':   'store',
-  'cart':    'cart',
-  'login':   'login',
-  'signup':  'signup',
-  'news':    'news',
-  'players': 'players',
+  '':           'home',
+  'home':       'home',
+  'esports':    'esports',
+  'basketball': 'basketball',
+  'football':   'football',
+  'news':       'news',
+  'matches':    'matches',
+  'store':      'store',
+  'search':     'search',
+  'cart':       'cart',
+  'login':      'login',
+  'signup':     'signup',
 };
+
 const pick = (h) => routes[h.replace(/^#\/?/, '')] ?? 'home';
 
 export const getRoute = () => pick(location.hash);
